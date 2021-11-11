9a64e66e7fd0c97c08a23788ff30a3a35ac67d8d
Jefferson Augusto Goya <jeff.goya@gmail.com>
2021-11-11 16:20:18 -0300

    Add files via upload

---
+++ authentication-app-vue/src/components/auth-components/home/HomeComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand">JWT + Node + Vue (Jefferson Augusto Goya)</a>
+
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <button class="btn btn-primary w-1000 btn-sm" @click="logOutUser">
+                Sair
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <section>
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="list-group">
+              <li class="list-group-item">Nam e: {{ user.name }}</li>
+              <li class="list-group-item">E-mail: {{ user.email }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script src="./Home.js"></script>
+
+<style src="" scoped>
+</style>
